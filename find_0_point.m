ba9fb478f955c5aa6f512cf204970aa67f7a0671
FLYR01 <3223278465@qq.com>
2022-12-20 12:38:55 +0800

    annotation update

--- find_0_point.m
+++ find_0_point.m
@@ -1,4 +1,8 @@
 function [izero,all_x0,min_x0]=find_0_point(x,y)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%find the the zero ponit of y=f(x), i.e the position of the different sign
+% NOTE: x and y are both column vector
+
 intervals=(y<=0)-(y>0);
 izerosup=find(diff(intervals)<0);
 intervals=(y>0)-(y<=0);
