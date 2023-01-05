b3029abaa0e2a710b0030fb07306379087cf3c25
FLYR01 <3223278465@qq.com>
2023-01-05 10:34:25 +0800

    single all pass ring

--- all_state_calculation.m
+++ all_state_calculation.m
@@ -6,15 +6,15 @@ startdelta=0.00;
 enddelta=0.025;
 aj=10;
 rk=10;
-a=linspace(0.90,0.98,10);
-r=linspace(0.89,0.97,10);
+a=0.97
+r=0.94
 % L=4.274761274761274e-05% L is obtained through the resonace condition of the MRR
 % L=1.825128251282513e-05% L is obtained through the resonace condition of the MRR
 % L=5.235382353823538e-05;
 L=5.043260043260043e-05;
 neff_file_name='neff1000.csv';%the name of the effective index file
 ng_file_name='ng1000.csv';%the name of the group index file
-maxchannel=8;% the number of wavelength channel
+maxchannel=1;% the number of wavelength channel
 load('available_L.mat')
 
 pks_bar=zeros(125,maxchannel);
@@ -29,10 +29,10 @@ bottom4=zeros(aj,rk,125);
 peak4=zeros(aj,rk,125);
 FWHM4=zeros(aj,rk,125);
 
-for j=1:1:aj
-    for k=1:1:rk
+for j=1:1:1
+    for k=1:1:1
         if (a(j)-r(k)>0.0001)
-            for i=41:1:84
+            for i=66:1:66
                I=i
                L=all_L(i);% L is obtained through the resonace condition of the MRR
                [ERER4(j,k,i),peak4(j,k,i),bottom4(j,k,i),FWHM4(j,k,i),switch_state_E_out(i,:,:)]=all_switch_state_parameters(resolution,a(j),r(k),L,neff_file_name,ng_file_name,maxchannel);
